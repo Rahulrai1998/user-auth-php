3c5bfa570c9f8a01a2e98e1d1f353848b396efbf
Rahul Kumar <rahulrai2822march@gmail.com>
2025-05-12 21:39:37 +0530

    mysql connection done

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$HOSTNAME = "localhost";
+$USERNAME = "root";
+$PASSWORD = "";
+$DATABASE = "user-auth";
+
+$connection = mysqli_connect($HOSTNAME, $USERNAME, $PASSWORD, $DATABASE);
+
+if (!$connection) {
+    die("Connection Error " . mysqli_connect_error($connection));
+}
+echo "Connection Successful";
